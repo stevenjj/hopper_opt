fa976fe998d2cdb91b162d3fbc0d71e66940315c
Steven Jens Jorgensen <stevenjensj@gmail.com>
2018-03-22 17:13:09 -0500

    add opt problem header

---
+++ include/hdt/optimization_problems/opt_problem_main.hpp
@@ -0,0 +1,35 @@
+#ifndef OPTIMIZATION_PROBLEM_MAIN_H
+#define OPTIMIZATION_PROBLEM_MAIN_H
+#include <Utils/wrap_eigen.hpp>
+#include <hdt/optimization_constants.hpp>
+#include <hdt/containers/opt_variable_manager.hpp>
+#include <string>
+
+class Optimization_Problem_Main{
+public:
+  Optimization_Problem_Main(){}
+  virtual ~Optimization_Problem_Main(){}
+
+  std::string problem_name = "Undefined Optimization Problem";
+
+  virtual void get_init_opt_vars(std::vector<double> &x_vars){}
+  virtual void get_opt_vars_bounds(std::vector<double> &x_low, std::vector<double> &x_upp){}   	  	
+  virtual void get_current_opt_vars(std::vector<double> &x_vars_out){}
+  virtual void update_opt_vars(std::vector<double> &x_vars){} 	  		
+
+  virtual void get_var_manager(Opt_Variable_Manager* &var_manager_out){}
+
+  virtual void get_F_bounds(std::vector<double> &F_low, std::vector<double> &F_upp){}
+  virtual void get_F_obj_Row(int &obj_row){}
+
+  virtual void compute_F(std::vector<double> &F_eval){}
+  virtual void compute_F_constraints(std::vector<double> &F_eval){}
+  virtual void compute_F_objective_function(double &result_out){}
+
+  virtual void compute_G(std::vector<double> &G_eval, std::vector<int> &iGfun, std::vector<int> &jGvar, int &neG){}
+  virtual void compute_A(std::vector<double> &A_eval, std::vector<int> &iAfun, std::vector<int> &jAvar, int &neA){}
+
+
+};
+
+#endif
